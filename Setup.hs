3e48cc72973367d0048291a9f5f455e6ce8ad05a
Brian Muramatsu <btmura@gmail.com>
2010-12-17 23:58:38 -0800

    Add User Hook for Tests
    Switch to a custom Cabal build to add a user hook for "cabal test"
    that will run the "b1-tests" executable. Move the main source code
    to "src" and create a new directory for tests called "tests."

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,24 @@
+import Distribution.PackageDescription
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.UserHooks
+
+import System.Directory
+import System.FilePath
+import System.Process
+
+testSuiteExe = "b1-tests"
+
+main :: IO ()
+main = defaultMainWithHooks hooks
+
+hooks :: UserHooks
+hooks = simpleUserHooks { runTests = runTestSuite }
+
+runTestSuite :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
+runTestSuite _ _ _ localBuildInfo = do
+  let testDir = buildDir localBuildInfo </> testSuiteExe
+  setCurrentDirectory testDir
+  exitCode <- system testSuiteExe
+  return ()
+
