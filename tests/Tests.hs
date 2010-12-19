7a575c41fa02dec836608d73b262b88e35fd3e5f
Brian Muramatsu <btmura@gmail.com>
2010-12-18 22:45:58 -0800

    Add Sample QuickCheck Test

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,3 +1,17 @@
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+
+import Data.List
+
 main :: IO ()
-main = putStrLn "Running the tests!"
+main = defaultMain tests
+
+tests =
+  [ testGroup "Test Group 1"
+    [ testProperty "sort1" prop_sort1
+    ]
+  ]
+
+prop_sort1 xs = sort xs == sortBy compare xs
+  where types = (xs :: [Int])
 
