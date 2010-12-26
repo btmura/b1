cf1c4e219c2230b36c1838a7c90299055a78708f
Brian Muramatsu <btmura@gmail.com>
2010-12-25 22:34:13 -0800

    Add Spinning Quad Demo

--- src/B1/Program/Chart/Main.hs
+++ src/B1/Program/Chart/Main.hs
@@ -10,7 +10,7 @@ main :: IO ()
 main = do
   initialize
   createWindow
-  drawLoop
+  drawLoop $ Just (drawAction 0)
   closeWindow
   terminate
 
@@ -24,17 +24,42 @@ myWindowSizeCallback :: Size -> IO ()
 myWindowSizeCallback size@(Size width height) = do
   viewport $= (Position 0 0, size)
 
-drawLoop :: IO ()
-drawLoop = do
-  drawScene
-  swapBuffers
+data Action = Action (Maybe (IO Action))
 
-  esc <- getKey ESC
-  unless (esc == Press) $ do
-    sleep 0.001
-    drawLoop
+drawLoop :: Maybe (IO Action) -> IO ()
+drawLoop maybeAction = do
+  case maybeAction of
+    Just action -> do
+      Action nextAction <- action
+      drawLoop nextAction
+    _ -> return ()
 
-drawScene :: IO ()
-drawScene = do
+drawAction :: GLfloat -> IO Action
+drawAction rotateY = do
   clear [ColorBuffer, DepthBuffer]
   loadIdentity
+
+  doScale 0.5 0.5 1.0
+  doRotate rotateY 0 1.0 0
+  renderPrimitive LineLoop $ do
+    drawVertex2 (-1) (-1)
+    drawVertex2 (-1) 1
+    drawVertex2 1 1
+    drawVertex2 1 (-1)
+
+  swapBuffers
+  sleep 0.001
+
+  esc <- getKey ESC
+  if esc == Press
+    then return $ Action Nothing
+    else return $ Action $ Just $ drawAction $ rotateY + 0.2
+    
+doScale :: GLfloat -> GLfloat -> GLfloat -> IO ()
+doScale x y z = scale x y z
+
+doRotate :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> IO ()
+doRotate c x y z = rotate c $ Vector3 x y z
+
+drawVertex2 :: GLfloat -> GLfloat -> IO () 
+drawVertex2 x y = vertex $ Vertex2 x y
