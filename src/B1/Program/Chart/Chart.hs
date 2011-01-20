eda2247aeddefe4cb4b3791f302f2b9746b9490b
Brian Muramatsu <btmura@gmail.com>
2011-01-19 22:58:18 -0800

    More Animation Tuning
    - Remove the motion animation when entering symbols
    - Vary opacity and scale of initial animation

--- src/B1/Program/Chart/Chart.hs
+++ src/B1/Program/Chart/Chart.hs
@@ -19,8 +19,8 @@ import B1.Program.Chart.Resources
 data ChartState = ChartState
   { currentSymbol :: String
   , nextSymbol :: String
-  , initAnimation :: Animation (GLfloat, Dirty)
-  , nextSymbolAnimations :: [Animation (GLfloat, Dirty)]
+  , entryScaleAnimation :: Animation (GLfloat, Dirty)
+  , entryAlphaAnimation :: Animation (GLfloat, Dirty)
   } deriving (Show)
 
 drawChart :: Resources -> IO (Action Resources Dirty, Dirty)
@@ -29,8 +29,8 @@ drawChart resources = drawChartLoop initState resources
     initState = ChartState
       { currentSymbol = ""
       , nextSymbol = ""
-      , initAnimation = animateOnce $ gradualRange 0 1 100
-      , nextSymbolAnimations = []
+      , entryScaleAnimation = animateOnce $ linearRange 0.5 1 50
+      , entryAlphaAnimation = animateOnce $ linearRange 0 1 50
       }
 
 drawChartLoop :: ChartState -> Resources -> IO (Action Resources Dirty, Dirty)
@@ -39,10 +39,12 @@ drawChartLoop state resources = do
   translate $ vector3 (sideBarWidth resources + (mainChartWidth resources) / 2)
       (mainChartHeight resources / 2) 0
 
-  let (animationValue, _) = getCurrentFrame (initAnimation state)
-  scale3 animationValue 1 1
+  let scaleAmount = fst . getCurrentFrame . entryScaleAnimation $ state
+  scale3 scaleAmount scaleAmount 1
 
-  color $ color4 0.25 1 0 animationValue
+  let alphaAmount = fst . getCurrentFrame . entryAlphaAnimation $ state
+  color $ color4 0.25 1 0 alphaAmount
+
   let newState = refreshSymbolState state resources
 
   when (currentSymbol newState /= "") $
@@ -54,7 +56,7 @@ drawChartLoop state resources = do
   when (nextSymbol newState /= "") $
     drawNextSymbol newState resources
 
-  color $ color4 0 0.25 1 animationValue
+  color $ color4 0 0.25 1 alphaAmount
   drawChartBorder resources
 
   return (Action (drawChartLoop (refreshAnimationState newState)),
@@ -69,16 +71,9 @@ mainChartHeight resources = windowHeight resources
 refreshSymbolState :: ChartState -> Resources -> ChartState
 
 -- Append to the next symbol if the key is just a character...
-refreshSymbolState state@ChartState
-    { nextSymbol = nextSymbol 
-    , nextSymbolAnimations = nextSymbolAnimations
-    }
+refreshSymbolState state@ChartState { nextSymbol = nextSymbol }
     (Resources { keyPress = Just (CharKey char) })
-  | isAlpha char = state
-      { nextSymbol = nextSymbol ++ [char]
-      , nextSymbolAnimations = nextSymbolAnimations
-          ++ [newSymbolLetterAnimation]
-      }
+  | isAlpha char = state { nextSymbol = nextSymbol ++ [char] }
   | otherwise = state
 
 -- ENTER makes the next symbol the current symbol.
@@ -86,21 +81,14 @@ refreshSymbolState state@ChartState { nextSymbol = nextSymbol }
     (Resources { keyPress = Just (SpecialKey ENTER) }) = state
   { currentSymbol = nextSymbol
   , nextSymbol = ""
-  , nextSymbolAnimations = []
   }
 
 -- ESC cancels the next symbol.
-refreshSymbolState state
-    (Resources { keyPress = Just (SpecialKey ESC) }) = state
-  { nextSymbol = ""
-  , nextSymbolAnimations = []
-  }
+refreshSymbolState state (Resources { keyPress = Just (SpecialKey ESC) }) =
+  state { nextSymbol = "" }
 
 -- Drop all other events.
 refreshSymbolState state _ = state
-
-newSymbolLetterAnimation :: Animation (GLfloat, Dirty)
-newSymbolLetterAnimation = animateOnce $ gradualRange 5 1 100
 
 chartPadding :: GLfloat
 chartPadding = 10
@@ -156,13 +144,8 @@ drawCurrentSymbol symbol resources = do
   destroyLayout layout
 
 drawNextSymbol :: ChartState -> Resources -> IO ()
-drawNextSymbol (ChartState
-    { nextSymbol = nextSymbol
-    , nextSymbolAnimations = nextSymbolAnimations
-    })
+drawNextSymbol (ChartState { nextSymbol = nextSymbol })
     resources = do
-
-  let (animationValue, _) = getCurrentFrame $ last nextSymbolAnimations
 
   layout <- createSimpleLayout
   setFontFaceSize (font resources) 48 72
@@ -176,26 +159,25 @@ drawNextSymbol (ChartState
       centerY = -(top - (abs (bottom - top)) / 2)
   preservingMatrix $ do 
     translate $ vector3 centerX centerY 0
-    scale3 animationValue animationValue animationValue
     renderLayout layout nextSymbol
 
   destroyLayout layout
 
 refreshAnimationState :: ChartState -> ChartState
 refreshAnimationState state@ChartState
-    { initAnimation = initAnimation
-    , nextSymbolAnimations = nextSymbolAnimations
+    { entryScaleAnimation = entryScaleAnimation
+    , entryAlphaAnimation = entryAlphaAnimation
     } = state
-    { initAnimation = getNextAnimation initAnimation
-    , nextSymbolAnimations = map getNextAnimation nextSymbolAnimations
+    { entryScaleAnimation = getNextAnimation entryScaleAnimation
+    , entryAlphaAnimation = getNextAnimation entryAlphaAnimation
     }
 
 isDirty :: ChartState -> Bool
 isDirty (ChartState
-    { initAnimation = initAnimation
-    , nextSymbolAnimations = nextSymbolAnimations
+    { entryScaleAnimation = entryScaleAnimation
+    , entryAlphaAnimation = entryAlphaAnimation
     }) = any (snd . getCurrentFrame) allAnimations
   where
-    allAnimations = initAnimation:nextSymbolAnimations
+    allAnimations = [entryScaleAnimation, entryAlphaAnimation]
 
 
