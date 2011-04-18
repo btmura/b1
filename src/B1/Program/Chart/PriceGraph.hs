c4f45987d97d7f6500780c92bd814bcf175047d8
Brian Muramatsu <btmura@gmail.com>
2011-04-17 21:37:40 -0700

    Draw Ugly Bars for Price Graph

--- src/B1/Program/Chart/PriceGraph.hs
+++ src/B1/Program/Chart/PriceGraph.hs
@@ -8,6 +8,7 @@ module B1.Program.Chart.PriceGraph
 
 import Graphics.Rendering.OpenGL
 
+import B1.Data.Price
 import B1.Graphics.Rendering.OpenGL.Box
 import B1.Graphics.Rendering.OpenGL.Shapes
 import B1.Graphics.Rendering.OpenGL.Utils
@@ -40,6 +41,7 @@ newPriceGraphState stockData =
 drawPriceGraph :: Resources -> PriceGraphInput -> IO PriceGraphOutput
 drawPriceGraph resources input = 
   convertInputToStuff input
+      >>= renderPriceGraph
       >>= convertStuffToOutput
 
 data PriceGraphStuff = PriceGraphStuff
@@ -64,6 +66,82 @@ convertInputToStuff
     , priceStockData = stockData
     , priceIsDirty = False
     }
+
+renderPriceGraph :: PriceGraphStuff -> IO PriceGraphStuff
+renderPriceGraph
+    stuff@PriceGraphStuff
+      { priceStockData = stockData
+      } = do
+  maybePriceData <- getStockPriceData stockData
+  maybe (return stuff { priceIsDirty = True })
+      (\priceData -> either
+          (renderGraph stuff)
+          (renderError stuff)
+          (pricesOrError priceData))
+      maybePriceData
+
+renderGraph :: PriceGraphStuff -> [Price] -> IO PriceGraphStuff
+renderGraph
+    stuff@PriceGraphStuff
+      { priceBounds = bounds
+      , priceAlpha = alpha
+      }
+    prices =
+  preservingMatrix $ do
+    mapM_ (renderBar alpha) $ getBars bounds prices
+    return stuff { priceIsDirty = False }
+
+data Bar = Bar
+  { translateX :: GLfloat
+  , translateY :: GLfloat
+  , barWidth :: GLfloat
+  , barHeight :: GLfloat
+  }
+
+renderBar :: GLfloat -> Bar -> IO ()
+renderBar alpha bar =
+  preservingMatrix $ do
+    translate $ vector3 (translateX bar) (translateY bar) 0
+    scale3 (barWidth bar / 2) (barHeight bar / 2) 1
+    renderPrimitive Quads $ do
+      vertex $ vertex2 (-1) (-1)
+      vertex $ vertex2 (-1) 1
+      vertex $ vertex2 1 1
+      vertex $ vertex2 1 (-1)
+
+getBars :: Box -> [Price] -> [Bar]
+getBars bounds prices = map (createBar bounds prices) [0 .. length prices - 1]
+
+createBar :: Box -> [Price] -> Int -> Bar
+createBar bounds prices index = Bar
+  { translateX = translateX
+  , translateY = translateY
+  , barWidth = barWidth
+  , barHeight = barHeight
+  }
+  where
+    translateX = boxWidth bounds / 2 - barWidth / 2
+        - barWidth * realToFrac index
+    translateY = -(boxHeight bounds / 2) + barHeight / 2
+        + lowerHeight
+
+    numPrices = length prices
+    barWidth = boxWidth bounds / realToFrac numPrices
+    maxPrice = maximum $ map high prices
+    minPrice = minimum $ map low prices
+    totalRange = maxPrice - minPrice
+
+    price = prices !! index
+    priceRange = high price - low price
+    heightPercentage = priceRange / totalRange
+    barHeight = boxHeight bounds * realToFrac heightPercentage
+
+    lowerRange = low price - minPrice
+    lowerHeightPercentage = lowerRange / totalRange
+    lowerHeight = boxHeight bounds * realToFrac lowerHeightPercentage
+
+renderError :: PriceGraphStuff -> String -> IO PriceGraphStuff
+renderError stuff error = return stuff { priceIsDirty = False }
 
 convertStuffToOutput :: PriceGraphStuff -> IO PriceGraphOutput
 convertStuffToOutput
